451c97d1192552f33fa5469c2390b00f468f9bea
JustinLeeVance <justinleevance94@gmail.com>
2023-12-19 11:49:58 -0500

    Rendered the Img's to Screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+fetch('http://localhost:3000/ramens').then((resp) => resp.json()).then((data) => renderRamen(data));
+
+const div = document.querySelector("#ramen-menu")
+// console.log(div)
+function renderRamen(data){
+    data.forEach((ramenObj) => {
+        const imgTag = document.createElement("img");
+        // console.log(imgTag)
+        imgSrc = ramenObj.image
+        // console.log(imgSrc)
+        imgTag.src = imgSrc
+        const li = document.createElement("li");
+        li.appendChild(imgTag);
+        div.appendChild(li);
+        })
+    const ramenName = document.querySelector(".name")
+    console.log(ramenName);
+    const ramenRestaurant = document.querySelector(".restaurant");
+    console.log(ramenRestaurant);
+
+
+}
